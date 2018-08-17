5b58971a04b0951ef9139cad15c6bbf7866fc67c
Laurens Mertens <laurens@keypuncher.be>
2018-08-17 19:53:55 +0200

    correcting backend url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true
 };
 
-export const BACKEND_URL = 'https://hoevewinkel-backend.herokuapp.com/';
+export const BACKEND_URL = 'https://hoevewinkel-backend.herokuapp.com';
